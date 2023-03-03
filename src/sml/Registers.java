5dcd06be7854cd333251364984cf014c8e5a7544
Anna Rozensteina <anna.margreta.r@gmail.com>
2023-03-03 18:41:31 +0000

    Javadoc for Registers.

--- src/sml/Registers.java
+++ src/sml/Registers.java
@@ -3,23 +3,30 @@ package sml;
 import java.util.*;
 import java.util.stream.Collectors;
 
-// TODO: write a JavaDoc for the class
-
 /**
+ * The Registers class represents registers that can be used in the program.
+ * Registers are represented as a HashMap where each unique register has a name
+ * and a value.
  *
- * @author ...
+ * @author BBK, arozen01
  */
 public final class Registers {
     private final Map<Register, Integer> registers = new HashMap<>();
 
     public enum Register implements RegisterName {
-        EAX, EBX, ECX, EDX, ESP, EBP, ESI, EDI;
+        EAX, EBX, ECX, EDX, ESP, EBP, ESI, EDI
     }
 
+    /**
+     * Calls clear method which creates all registers and clears them by setting them to 0.
+     */
     public Registers() {
         clear(); // the class is final
     }
 
+    /**
+     * Creates all registers and clears them by setting them to 0.
+     */
     public void clear() {
         for (Register register : Register.values())
             registers.put(register, 0);
@@ -45,8 +52,12 @@ public final class Registers {
         return registers.get((Register)register);
     }
 
-    // pattern matching for instanceof
-    // https://docs.oracle.com/en/java/javase/14/language/pattern-matching-instanceof-operator.html
+    /**
+     * Compares two instances.
+     *
+     * @param o object to be compared with
+     * @return true if o is equal to an instance of Registers, otherwise false
+     */
     @Override
     public boolean equals(Object o) {
         if (o instanceof Registers other) {
@@ -60,6 +71,13 @@ public final class Registers {
         return registers.hashCode();
     }
 
+    /**
+     * String representation of the registers.
+     * <p>
+     * Form: [register = value, register = value, ... , register = value]
+     *
+     * @return pretty formatted version of the code.
+     */
     @Override
     public String toString() {
         return registers.entrySet().stream()
